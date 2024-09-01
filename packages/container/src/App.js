54de6015fc39212682c377591b6d4e6c46dfb24d
JuliaTsitko <julia.tsitko@gmail.com>
2024-09-01 17:26:57 +0200

    Update container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
     return (
         <div>
-            <h1>Container Hi! Hello! Hi!</h1>
+            <h1>Container Hi!</h1>
             <hr />
             <MarketingApp />
         </div>
